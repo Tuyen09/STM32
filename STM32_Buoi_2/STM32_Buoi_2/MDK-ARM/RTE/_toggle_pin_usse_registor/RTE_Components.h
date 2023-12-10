4206409a43c401af71136bd20433c7c05dbd0a38
tuyen <pdf04082002@gmail.com>
2023-12-10 23:09:28 +0700

    STM32_Buoi_2

---
+++ STM32_Buoi_2/STM32_Buoi_2/MDK-ARM/RTE/_toggle_pin_usse_registor/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'toggle_pin_usse_registor' 
+ * Target:  'toggle_pin_usse_registor' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f4xx.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
---
+++ STM32_Buoi_2/STM32_Buoi_2/MDK-ARM/toggle_pin_usse_registor/startup_stm32f411xe.d
@@ -0,0 +1,1 @@
+toggle_pin_usse_registor\startup_stm32f411xe.o: startup_stm32f411xe.s
